54da07346996d2906eed67ecadd929a5b1a01daa
Vika Nika <49692236+vikanika66@users.noreply.github.com>
2019-05-31 01:29:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,16 +9,13 @@ process_par = 1
 num_channels = 6
 
 
-#arr_devices.append(1)
-#arr_devices.pop(0)
-
 def sim_input ():
-    return -math.log(random.random())/input_par
+    return -math.log(random.random())/input_par # Моделируем время прихода следующей заявки
 
 def sim_proc ():
-    return -math.log(random.random())/process_par
+    return -math.log(random.random())/process_par # Моделируем время обработки заявки
 
-def get_n_realiz(num_tests):
+def get_n_realiz(num_tests): # Моделируем num_tests траекторий
     sum = 0
     i=0
     while i < num_tests:
@@ -28,29 +25,34 @@ def get_n_realiz(num_tests):
 
 def simulate_func():
     
-    t_now = 0
-    arr_devices = np.zeros(num_channels)
-    arr_queque = []
-    result = {'processed' : 0, 'wait_time_total' : 0}
+    t_now = 0 # Начинаем в 0
+    arr_devices = np.zeros(num_channels) # Создаем массив каналов
+    arr_queque = [] # Очередь пустая
+    result = {'processed' : 0, 'wait_time_total' : 0} # Результат пустой
     
-    while t_now < t_max:
+    while t_now < t_max: # Пока не вышли за границы
         
-        t_now +=  sim_input()
-        while np.amin(arr_devices) < t_now and len(arr_queque) > 0:
-            
-            free_dev = np.argmin(arr_devices)
+        t_now +=  sim_input() # Текущее время - время прихода очередной заявки
+        
+        while np.amin(arr_devices) < t_now and len(arr_queque) > 0: # Пока заявки не приходили приборы успели что-то обработать
+                                                                    # учтем это
+            free_dev = np.argmin(arr_devices) # Номер свободного прибора
             
             result['processed'] += 1
-            result['wait_time_total'] += arr_devices[free_dev] - arr_queque[0]
-            
-            arr_devices[free_dev] += sim_proc()
-            arr_queque.pop(0)
-            
-        if np.amin(arr_devices) < t_now:
-            result['processed'] += 1
-            arr_devices[np.argmin(arr_devices)] += sim_proc()
+            result['wait_time_total'] += arr_devices[free_dev] - arr_queque.pop(0)  # Добавим информацию об обработаной заявке 
+                                                                                    # в результат и удалим ее из очереди
+            arr_devices[free_dev] += sim_proc() # При назначении заявки на канал моделируем время обработки и запоминаем 
+                                                # новое время освобождения канала
+                
+        if np.amin(arr_devices) < t_now: # Теперь разберемся с пришедшей заявкой
+            result['processed'] += 1 # Если канал свободен, то ставим на обслуживание
+            arr_devices[np.argmin(arr_devices)] = t_now + sim_proc() # Запоминаем новое время обработки
         else:
-            arr_queque.append(t_now)
+            arr_queque.append(t_now) # Если свободных нет, то добавляем в очередь
+    
+    for a in arr_queque: # Учтем время так и не обработанных заявок
+        result['wait_time_total'] += t_max - a
+        result['processed'] += 1
     
     return result['wait_time_total']/result['processed']
     
